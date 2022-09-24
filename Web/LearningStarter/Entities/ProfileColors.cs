fd9a34ec36e8f021eac90a603719f377fb1edfa6
QuackyDaddy <zachary.duncan@selu.edu>
2022-09-24 17:39:28 -0500

    Feat-GroupMembers/ProfileColors/Groups

---
+++ Web/LearningStarter/Entities/ProfileColors.cs
@@ -0,0 +1,14 @@
+﻿namespace LearningStarter.Entities
+{
+    public class ProfileColors
+    {
+        public int Id { get; set; }
+        public int Colors { get; set; }
+    }
+    public class ProfileColorsGetDto
+    {
+        public int Id { get; set; }
+        public int Colors { get; set; }
+    }
+}
+  
