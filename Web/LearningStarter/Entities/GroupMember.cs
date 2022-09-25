08a1e1d1c08102694decac1b8304ede69b7f39e1
Robin White <1whiter2000@gmail.com>
2022-09-24 19:55:42 -0500

    Feat: Changed Entities and Context to Singular

--- Web/LearningStarter/Entities/GroupMember.cs
+++ Web/LearningStarter/Entities/GroupMember.cs
@@ -1,13 +1,13 @@
 ﻿namespace LearningStarter.Entities
 {
-    public class GroupMembers
+    public class GroupMember
     {
         public int Id { get; set; }
         public string RoleId { get; set; }
         public int UserId { get; set; } 
         public int GroupId { get; set; }
     }
-    public class GroupMemebersGetDto {      
+    public class GroupMemeberGetDto {      
         public int Id { get; set; }    
         public int UserId { get; set; } 
         public string RoleId { get; set; }  
