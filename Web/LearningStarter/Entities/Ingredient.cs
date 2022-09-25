0f372e1061fb7fabef36d021d197e22235defdaa
Cole-Bailey <cole.bailey@selu.edu>
2022-09-24 20:49:36 -0500

    Cole/ReAdded-Recipe-Unit

---
+++ Web/LearningStarter/Entities/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿namespace LearningStarter.Entities
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int UnitId  { get; set; }
+    }
+    public class IngredientGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int UnitId { get; set; }
+    }
+    public class IngredientCreateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int UnitId { get; set; }
+    }
+    public class IngredientUpdateDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int UnitId { get; set; }
+    }
+}
---
+++ Web/LearningStarter/Entities/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningStarter.Entities
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+
+    }
+}
