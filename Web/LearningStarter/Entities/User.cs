dd3f0a7de03e19407bf28db0ebe81be2f58cacc5
cazcheramie <caz.cheramie@selu.edu>
2022-09-25 14:36:09 -0500

    Feats: Fixed User class

--- Web/LearningStarter/Entities/User.cs
+++ Web/LearningStarter/Entities/User.cs
@@ -1,7 +1,74 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace LearningStarter.Entities
 {
+    public class User
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public int ProfileColorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTimeOffset BirthDay { get; set; }
+
+    }
+
+    public class UserCreateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserUpdateDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserGetDto
+    {
+        public int Id { get; set; }
+        public int ProfileColorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public DateTimeOffset BirthDay { get; set; }
+    }
+}
+
+/*using System;
+
+namespace LearningStarter.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int ProfileColorId { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset BirthDay { get; set; }
+    }
     public class UserGetDto
     {
         public int Id { get; set; }
@@ -13,5 +80,27 @@ namespace LearningStarter.Entities
         public string Email { get; set; }
         public DateTimeOffset BirthDay { get; set; }
 
+        public class UserCreateDto
+        {
+            public int Id { get; set; }
+            public int ProfileColorId { get; set; }
+            public string Name { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public int PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public DateTimeOffset BirthDay { get; set; }
+        }
+        public class UserUpdateDto
+        {
+            public int ProfileColorId { get; set; }
+            public string Name { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public int PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public DateTimeOffset BirthDay { get; set; }
+        }
     }
 }
+*/
