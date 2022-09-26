063b09aa9909adf5f4a34fd4e4b35e6a4cb921a8
Robin White <1whiter2000@gmail.com>
2022-09-26 11:57:58 -0500

    Feats: Ingredients Controller Complete, Added Dto's to Ingredient, Recipe, Unit, and Calendar, Seeded Units, Ingredients, Recipe

--- Web/LearningStarter/Entities/Calendar.cs
+++ Web/LearningStarter/Entities/Calendar.cs
@@ -5,11 +5,25 @@ namespace LearningStarter.Entities
     public class Calendar
     {
         public int Id { get; set; }
+        public Group Group { get; set; }
         public int GroupId { get; set; }
     }
     public class CalendarGetDto
     {
         public int Id { get; set; }
+        public Group Group{ get; set; }
+        public int GroupId { get; set; }
+    }
+
+    public class CalendarCreateDto
+    {
+        public Group Group { get; set; }
+        public int GroupId { get; set; }
+    }
+
+    public class CalendarUpdateDto
+    {
+        public Group Group { get; set; }
         public int GroupId { get; set; }
     }
 }
--- Web/LearningStarter/Entities/Unit.cs
+++ Web/LearningStarter/Entities/Unit.cs
@@ -9,6 +9,26 @@ namespace LearningStarter.Entities
         public string Abbreviation { get; set; }
         public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
 
+    }
 
+    public class UnitGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
+
+    public class UnitCreateDto
+    {
+       
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+    }
+
+    public class UnitUpdateDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
     }
 }
