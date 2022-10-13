f729fc18f02235d48f9aa864a4c6f7fa155eede4
Robin White <1whiter2000@gmail.com>
2022-10-13 09:20:40 -0500

    Feat: Updated Controllers, Entities, and Startup
    Removed Units from Ingredients Controller, Updated the Recipes Controller, Fixed the Users controller to working condition, fixed a missing variable in Profile Colors Entity, made changes to User Entity to work with controller, and corrected seeding for User in StartUp.

--- Web/LearningStarter/Entities/User.cs
+++ Web/LearningStarter/Entities/User.cs
@@ -11,6 +11,7 @@ namespace LearningStarter.Entities
         [JsonIgnore]
         public int Id { get; set; }
         public int ProfileColorId { get; set; }
+        public ProfileColor ProfileColor { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
@@ -21,91 +22,39 @@ namespace LearningStarter.Entities
         public string DateInput { get; set; }
     }
 
+    public class UserGetDto
+    {
+        public int Id { get; set; }
+        public int ProfileColorId { get; set; }
+        public ProfileColorGetDto ProfileColor { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string BirthDay { get; set; }
+    }
+
     public class UserCreateDto
     {
+        public int ProfileColorId { get; set; }
+        public ProfileColorGetDto ProfileColor { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string DayOfMonth { get; set; }
-        public string Month { get; set; }
-        //public string BirthYear { get; set; }
         public string BirthDay { get; set; }
-        //public string DateInput { get; set; }
     }
 
     public class UserUpdateDto
     {
+        public int ProfileColorId { get; set; }
+        public ProfileColorGetDto ProfileColor { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }        
-    }
-
-    public class UserGetDto
-    {
-        public int Id { get; set; }
-        public int ProfileColorId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Username { get; set; }
-        public string BirthDay { get; set; }        
-    }
+        public string Email { get; set; } 
+    }   
 }
-
-/*using System;
-
-namespace LearningStarter.Entities
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public int ProfileColorId { get; set; }
-        public string Name { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public int PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public DateTimeOffset BirthDay { get; set; }
-    }
-    public class UserGetDto
-    {
-        public int Id { get; set; }
-        public int ProfileColorId { get; set; }
-        public string Name { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public int PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public DateTimeOffset BirthDay { get; set; }
-
-        public class UserCreateDto
-        {
-            public int Id { get; set; }
-            public int ProfileColorId { get; set; }
-            public string Name { get; set; }
-            public string UserName { get; set; }
-            public string Password { get; set; }
-            public int PhoneNumber { get; set; }
-            public string Email { get; set; }
-            public DateTimeOffset BirthDay { get; set; }
-        }
-        public class UserUpdateDto
-        {
-            public int ProfileColorId { get; set; }
-            public string Name { get; set; }
-            public string UserName { get; set; }
-            public string Password { get; set; }
-            public int PhoneNumber { get; set; }
-            public string Email { get; set; }
-            public DateTimeOffset BirthDay { get; set; }
-        }
-    }
-}
-*/
